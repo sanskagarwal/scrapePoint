8b3bcf8a0212db76f83b4d1bca63684c0f8d3b63
Sanskar <sanskaragarwal.500@gmail.com>
2019-05-01 19:53:58 +0530

    Customized user experience and code issue resolved

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SCRAPEPOINT
 
-ScrapePoint is a python script to convert [javatpoint](http://www.javatpoint.com) webpage to pdf. Currently it's not highly customizable but still does the job.
+ScrapePoint is a python script to convert [javatpoint](http://www.javatpoint.com) webpages to pdf. Finally it's highly customizable and does the job.
 
 
 ## Installation
@@ -13,21 +13,14 @@ Open scrapePoint directory.
 
 #### Before Running app.py install following dependencies first:
 
-=> For Windows Users
-```sh
-> pip install BeautifulSoup4
-> pip install pdfkit
-```
-Also, 
-Step 1: Install "wkhtmltopdf" package from [here](http://wkhtmltopdf.org/) accordingly to your machine.
-Step 2: Add PATH to environment variables or in line no. 75 in app.py add whole PATH to wkhtmltopdf.exe.
-        
-=> For Linux Users
+
 ```sh
 $ pip install BeautifulSoup4
 $ pip install pdfkit
-$ sudo apt-get install wkhtmltopdf
 ```
+Also, 
+Step 1: Install "wkhtmltopdf" package from [here](http://wkhtmltopdf.org/) accordingly to your machine. Linux Users needs to download static binary as some of the features are missing in package provided by linux repos.
+Step 2: For Windows => Add PATH to environment variables in line no. 155-156 in app.py add whole PATH to wkhtmltopdf.exe.
 
 After Installation of all packages run the following command
 ```sh
@@ -36,10 +29,10 @@ $ python app.py
 
 # Working
 
- - After running app.py, first of all it will request the webpage and get HTML of each module, it may take 2-4 mins
- - Then, it will convert each module to PDFs by the help of pdfkit and wkhtmltopdf packages in 30-40 seconds.
+ - After running app.py, first of all it will ask the user about the topic and modules, he/she wants to download.
+ - After this, it will request the webpage and get HTML of each module, it may take 2-4 mins depending upon length of whole topic
+ - Then, it will convert each module to PDFs by the help of pdfkit and wkhtmltopdf packages in 1-2 mins, again according to length of whole topic. Note: pdfkit ignores some files mainly images when a certain request timeout is achieved.
  - Project already consists of javatpoint.css to get better PDFs.
-
 
 ## License
 
--- app.py
+++ app.py
@@ -6,53 +6,134 @@ from bs4 import BeautifulSoup
 import os
 import pdfkit
 
+# Getting All site Information
+
+http = urllib3.PoolManager()
+url = "https://www.javatpoint.com"
+r = http.request('GET', url)
+soup = BeautifulSoup(r.data, 'html.parser')
+allContent = soup.findAll('fieldset', attrs={'class': 'gra1'})
+
+# Manipulating Site Information 
+
+fieldNames = []
+topics = []
+for field in allContent:
+    headline = field.find('h2').text
+    fieldNames.append(headline)
+    contentNames = field.findAll('p')
+    contentLinks = field.findAll('a')
+    fields = []
+    for i in range(len(contentNames)):
+        if(contentLinks[i]['href'][0:4]!='http'):
+            contentLinks[i]['href'] = 'http://www.javatpoint.com/' + contentLinks[i]['href']
+        fields.append((contentNames[i].text, contentLinks[i]['href']))
+    topics.append(fields)
+    
+# Define User Choice
+    
+def getIndex(fieldNames):
+    try:
+        ind = int(input('Select one index: '))
+        if(ind>0 and ind<=len(fieldNames)):
+            return ind
+        else:
+            print('Please be a human!')
+            return getIndex(fieldNames)
+    except:
+        print('Please Select a number')
+        return getIndex(fieldNames)
+    
+# All Topics
+
+for i in range(len(fieldNames)):
+    print('{0}. {1}'.format(i+1, fieldNames[i]))
+    for x in topics[i]:
+        print('\t{0}'.format(x[0]))
+        
+    
+# Get User choice
+
+ind1 = getIndex(fieldNames)
+for i in range(len(topics[ind1-1])):
+    print('{0}. {1}'.format(i+1, topics[ind1-1][i][0]))
+ind2 = getIndex(topics[ind1-1])
+
+topicNameDecided = topics[ind1-1][ind2-1][0]
+topicLinkDecided = topics[ind1-1][ind2-1][1]
+print(topics[ind1-1][ind2-1][0], topics[ind1-1][ind2-1][1])
+
+# Getting Modules
+
+http = urllib3.PoolManager()
+url = topicLinkDecided
+r = http.request('GET', url)
+soup = BeautifulSoup(r.data, 'html.parser')
+
 # Creating Modules
-modules = ["java-tutorial","java-string","exception-handling-in-java","multithreading-in-java","java-io","java-awt"]
+
+moduleTags = soup.findAll('h2', attrs={'class': 'spanh2'})
+modules = []
+for module in moduleTags:
+    modules.append(module.text)
+
 for module in modules:
     if not os.path.exists(module):
         os.makedirs(module)
-
+        
 # Creating HTML files from each modules
 
-http = urllib3.PoolManager()
 reqNo=0
-for module in modules:
-    url = "http://www.javatpoint.com/" + module
-    r = http.request('GET', url)
-    soup = BeautifulSoup(r.data, 'html.parser')
-    contentTable = soup.findAll('div', attrs={'class': 'leftmenu'})
+contentTable = soup.findAll('div', attrs={'class': 'leftmenu'}) # Index
+
+for i in range(len(contentTable)):
     links = []
-    for i in range(len(contentTable)):
-            for x in contentTable[i]:
-                a = str(x)
-                y = "href="
-                i1 = a.find(y)
-                if(i1==-1):
-                    continue
-                i1+=6
-                i2 = a.find('\"', i1)
-                links.append(a[i1:i2])
+    for x in contentTable[i]:
+        a = str(x)
+        y = "href="
+        i1 = a.find(y)
+        if(i1==-1): # No Links
+            continue
+        i1+=6 # Skip <"a href=">
+        i2 = a.find('\"', i1)  # Find Ending quote
+        links.append(a[i1:i2]) # a[i1:i2] is the final href
+    print(links)
     for link in links:
         reqNo+=1
         print("Request Number:" + str(reqNo))
         url = "http://www.javatpoint.com/" + link
         r = http.request('GET', url)
         soup = BeautifulSoup(r.data, 'html.parser')
-        [s.extract() for s in soup('script')]
-        [s.extract() for s in soup('ins')]
-        [s.extract() for s in soup('iframe')]
+        [s.extract() for s in soup('script')]  # Remove Scripts
+        [s.extract() for s in soup('ins')]     # Remove Ads
+        [s.extract() for s in soup('iframe')]  # Remove Ads2
         mainContent = soup.find('div', attrs={'id': 'city'})
+
+        # For Images
+        images = mainContent.findAll('img')
+        for x in range(len(images)):
+            imageSrc = images[x]['src']
+            imageSrc = str(imageSrc)
+            if(imageSrc.find('static.javatpoint.com')==-1):
+                images[x]['src'] = 'http://javatpoint.com' + images[x]['src']
+
+        for x in range(len(images)):
+            mainContent.findAll('img')[x]['src']=images[x]['src']
+            
+        # For CodeBlock
+        codes = mainContent.findAll('textarea', attrs= {'name': 'code'})
+        for c in range(len(codes)):
+            newTag = soup.new_tag('p')
+            newTag.append(str(codes[c].get_text()))
+            mainContent.find('textarea').replaceWith(newTag)
+
+        # Final HTML
         mainContent = str(mainContent).replace("\n"," ")
-        pageLen = len(mainContent)
-        for i in range(pageLen-2):
-            if(mainContent[i]=='s' and mainContent[i+1]=='r' and mainContent[i+2]=='c' and mainContent[i+3]=='='):
-                mainContent = mainContent[:i+5] + "http://www.javatpoint.com/" + mainContent[i+5:]
-                i+=31
-        header = "<html> <head> <link rel=\"stylesheet\" type=\"text/css\" href=\"./../javatpoint.css\"> </head>  <body>"
+        header = "<html> <head> <meta charset='UTF-8'> <link rel=\"stylesheet\" type=\"text/css\" href=\"./../javatpoint.css\"> </head>  <body>"
         footer = "</body></html>"
         mainContent = header + mainContent + footer
         try:
-            f = open("./" + module + "/" + link + ".html", "w")
+            f = open("./" + modules[i] + "/" + link + ".html", "w")
             for ch in mainContent:
                 try:
                     f.write(ch)
@@ -70,15 +151,15 @@ def sorted_dir(folder):
         return os.path.getctime(path)
     return sorted(os.listdir(folder), key=getctime)
 
-modules = ["java-tutorial","java-string","exception-handling-in-java","multithreading-in-java","java-io","java-awt"]
+# For Windows only
+# path_wkthmltopdf = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
+# config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
-path_wkthmltopdf = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
-config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
 
 # Converting each module to PDF
 
-for module in modules:
-    myList = sorted_dir(module)
+for x in range(len(modules)):
+    myList = sorted_dir(modules[x])
     for i in range(len(myList)):
-        myList[i]=module + "/" + myList[i]
-    pdfkit.from_file(myList, module + '.pdf', configuration=config)
+        myList[i]=modules[x] + "/" + myList[i]
+    pdfkit.from_file(myList, modules[x] + '.pdf')
